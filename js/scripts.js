fe9a5ba3f9bd7d4810a1b7582b056c68fcaca468
Sabi K <sabik@sabik.com>
2017-03-24 23:13:57 +0100

    2 more comments in new repo (origin-1)

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,13 @@
 var name = prompt('Wpisz swoje imię');
 // alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
+
+/*
+console.log('Ta funkcja nie zadziała');
+*/
+
+console.log('Ta funkcja będzie działać');
+
+alert('Działam!');
+
+// alert('A ja nie');
